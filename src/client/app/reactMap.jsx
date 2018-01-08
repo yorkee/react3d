0bcb5000f64ed16faf6d03e7b3902958e5a2add9
Nancy Yeung <nancyyeung@booky.local>
2018-01-08 12:39:03 -0500

    using multiple react components

---
+++ src/client/app/reactMap.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+export default class ReactMap extends React.Component {
+  render () {
+    return (
+      <div>
+        <p> hahaha</p>
+      </div>
+    );
+  }
+}
+
---
+++ src/client/app/render.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {render} from 'react-dom';
+import ReactMap from './reactMap.jsx';
+import Test from './index.jsx';
+
+
+
+
+render(<ReactMap/>, document.getElementById('map'));
+render(<Test/>, document.getElementById('test'));
